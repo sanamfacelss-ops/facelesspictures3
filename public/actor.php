320232414b757df28c4cd8f6e726ef8caf1f684a
Mayur <sanamfacelss@gmail.com>
2026-06-27 14:10:03 +0530

    Add padding between video and script/lyrics image on actor cards

--- public/actor.php
+++ public/actor.php
@@ -287,7 +287,7 @@ function renderActorBriefCard(array $sc, string $fallbackBrief, bool $isSong = f
       <?php endif; ?>
     </div>
     <?php if ($imageUrl): ?>
-    <div class="card-sec" style="padding:0">
+    <div class="card-sec" style="padding:1rem 0 0">
       <div class="portrait-img-wrap" onclick="openImgLightbox('<?= addslashes(htmlspecialchars($imageUrl)) ?>')">
         <img src="<?= htmlspecialchars($imageUrl) ?>" alt="<?= $isSong ? 'Song lyrics' : 'Dialog script' ?>">
       </div>
